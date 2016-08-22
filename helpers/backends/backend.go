e9b4f3da4f3ef78c95477703f0b16fd847c14102
gotlium <gotlium@gmail.com>
2016-08-23 01:31:27 +0500

    enable lvm2 for local development

--- helpers/backends/backend.go
+++ helpers/backends/backend.go
@@ -3,13 +3,13 @@ package backends
 import (
 	"github.com/LPgenerator/Ldld/helpers/backends/zfs"
 	"github.com/LPgenerator/Ldld/helpers/backends/btrfs"
-	// "github.com/LPgenerator/Ldld/helpers/backends/lvm"
 	"github.com/LPgenerator/Ldld/helpers/backends/overlayfs"
+	"github.com/LPgenerator/Ldld/helpers/backends/lvm"
 )
 
 
 type Fs interface {
-    Snapshot(ct string, num int) map[string]string
+	Snapshot(ct string, num int) map[string]string
 	DumpFull(ct string, dst string) map[string]string
 	DumpIncr(ct1 string, prev string, ct2 string, curr string, dst string) map[string]string
 	Snapshots(ct string) map[string]string
@@ -26,18 +26,14 @@ type Fs interface {
 
 func New(backend string) (Fs) {
 	fs := []Fs{zfs.Zfs{}}
-
-
 	if backend == "btrfs" {
 		fs = []Fs{btrfs.Btrfs{}}
 	}
 	if backend == "overlayfs" {
 		fs = []Fs{overlayfs.Overlayfs{}}
 	}
-	/*
 	if backend == "lvm" {
 		fs = []Fs{lvm.Lvm{}}
 	}
-	*/
 	return fs[0]
 }
